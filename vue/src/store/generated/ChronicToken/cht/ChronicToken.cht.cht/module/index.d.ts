a478e608b486d2376a37370afac989f0fad44575
discoverdefiteam <discover.defi@gmail.com>
2022-03-13 05:58:41 +0000

    athena-1 testnet finalization

--- vue/src/store/generated/ChronicToken/cht/ChronicToken.cht.cht/module/index.d.ts
+++ vue/src/store/generated/ChronicToken/cht/ChronicToken.cht.cht/module/index.d.ts
@@ -1,14 +1,14 @@
 import { StdFee } from "@cosmjs/launchpad";
 import { Registry, OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgUpdateAdmin } from "./types/cht/tx";
-import { MsgIBCSend } from "./types/cht/ibc";
-import { MsgIBCCloseChannel } from "./types/cht/ibc";
-import { MsgExecuteContract } from "./types/cht/tx";
-import { MsgClearAdmin } from "./types/cht/tx";
-import { MsgStoreCode } from "./types/cht/tx";
 import { MsgInstantiateContract } from "./types/cht/tx";
+import { MsgExecuteContract } from "./types/cht/tx";
+import { MsgIBCSend } from "./types/cht/ibc";
+import { MsgStoreCode } from "./types/cht/tx";
 import { MsgMigrateContract } from "./types/cht/tx";
+import { MsgClearAdmin } from "./types/cht/tx";
+import { MsgIBCCloseChannel } from "./types/cht/ibc";
+import { MsgUpdateAdmin } from "./types/cht/tx";
 export declare const MissingWalletError: Error;
 export declare const registry: Registry;
 interface TxClientOptions {
@@ -20,14 +20,14 @@ interface SignAndBroadcastOptions {
 }
 declare const txClient: (wallet: OfflineSigner, { addr: addr }?: TxClientOptions) => Promise<{
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }?: SignAndBroadcastOptions) => any;
-    msgUpdateAdmin: (data: MsgUpdateAdmin) => EncodeObject;
-    msgIBCSend: (data: MsgIBCSend) => EncodeObject;
-    msgIBCCloseChannel: (data: MsgIBCCloseChannel) => EncodeObject;
-    msgExecuteContract: (data: MsgExecuteContract) => EncodeObject;
-    msgClearAdmin: (data: MsgClearAdmin) => EncodeObject;
-    msgStoreCode: (data: MsgStoreCode) => EncodeObject;
     msgInstantiateContract: (data: MsgInstantiateContract) => EncodeObject;
+    msgExecuteContract: (data: MsgExecuteContract) => EncodeObject;
+    msgIBCSend: (data: MsgIBCSend) => EncodeObject;
+    msgStoreCode: (data: MsgStoreCode) => EncodeObject;
     msgMigrateContract: (data: MsgMigrateContract) => EncodeObject;
+    msgClearAdmin: (data: MsgClearAdmin) => EncodeObject;
+    msgIBCCloseChannel: (data: MsgIBCCloseChannel) => EncodeObject;
+    msgUpdateAdmin: (data: MsgUpdateAdmin) => EncodeObject;
 }>;
 interface QueryClientOptions {
     addr: string;
